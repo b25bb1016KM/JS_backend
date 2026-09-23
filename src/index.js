2838c0f37219b174aea48b1a4a2714aeb2c2fbb4
Kaushiki Mukherjee <b25es1001@iitj.ac.in>
2026-09-24 00:57:40 +0530

    Added utils and some middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 //require("dotenv").config({path:'./.env'})
 import dotenv from "dotenv";
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
+
 import connectDB from "./db/db.js";
 
 dotenv.config({path: './.env'})
 
-connectDB()
+connectDB().then(()=> {
+  app.listen(process.env.PORT || 8000), () => {
+    console.log(`Server is running at port: ${process.env.PORT}`);
+  }
+})
+.catch((err)=> {
+  console.log("MONGODB connection failed!!", err);
+  
+})
 
 
 
